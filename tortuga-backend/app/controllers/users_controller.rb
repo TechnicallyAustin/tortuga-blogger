6019fb2210a46b1d0169e07cd1f5b8b70bc4e8a1
Austin Johnson <austinjohnson@Austins-MBP.lan>
2021-06-24 12:26:04 -0700

    Setting up my layout

--- tortuga-backend/app/controllers/users_controller.rb
+++ tortuga-backend/app/controllers/users_controller.rb
@@ -3,4 +3,28 @@ class UsersController < ApplicationController
         render json: {test: "success"}
     end
 
+    def index
+        user = User.find_by(id: params[:id])
+        render :json user
+
+    end
+
+
+  def new
+    user = User.new
+  end
+
+  def create 
+    user = User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+  
+
+
 end
--- tortuga-backend/app/models/user.rb
+++ tortuga-backend/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :blogs
+    has many :comments, through: :blogs
 end
+
+
