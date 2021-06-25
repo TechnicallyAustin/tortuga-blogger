e9615c59c5ee210d843edc4022e8c72fc0993e79
Austin Johnson <austinjohnson@Austins-MBP.lan>
2021-06-25 10:41:52 -0700

    Refactored fetches, added a basic event listener test for category

--- tortuga-frontend/src/api.js
+++ tortuga-frontend/src/api.js
@@ -2,14 +2,44 @@ const BACKEND_URL = 'http://localhost:3000';
 const COMMENTS_URL = `${BACKEND_URL}/comments`;
 const BLOG_URL = `${BACKEND_URL}/blogs`;
 const CATEGORIES_URL = `${BACKEND_URL}/categories`;
-const formattedData = response => response.json()
+const formattedData = response => response.json();
+
 
 function fetchCategories() {
     return fetch(CATEGORIES_URL)
     .then(formattedData)
+    .then(results => {
+        results.map(function(single){
+        let newCategory = addCategories(single)
+        console.log(newCategory)
+        })
+    })
+    // creates Category classes from each object 
 }
 
 function fetchComments() {
     return fetch(COMMENTS_URL)
     .then(formattedData)
+    // returns an array of objects 
+}
+
+function addCategories(data){
+    fromData = new Category(data.id, data.title)
+    console.log(fromData)
+    displayCategories(fromData)
+}
+
+function displayCategories(data) {
+    let ul = document.body.appendChild(document.createElement('ul')) // creats ul and assigns it to body 
+    let li = ul.appendChild(document.createElement('li')) // creates an li that is assigned to a ul
+    li.innerText = data.title
+}
+
+function categoryEventListeners() {
+     for(const singleLi of allLi){
+        singleLi.addEventListener('click', function(){
+        console.log("I was clicked")
+        })
+        } 
+
 }
--- tortuga-frontend/src/blog.js
+++
@@ -1,6 +0,0 @@
-// test that we can get data from the backend
-/*
-fetch(`${BACKEND_URL}/blogs`)
-  .then(response => response.json())
-  .then(jsonResponse => console.log(jsonResponse));
-  */
